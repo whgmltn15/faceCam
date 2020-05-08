783a7189f4bc4a6e073cf84741a8c04660dcff08
whgmltn15 <64678713+whgmltn15@users.noreply.github.com>
2020-05-08 22:29:05 +0900

    Add files via upload

---
+++ haarcascade_frontalface.py
@@ -0,0 +1,24 @@
+import cv2
+import time
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+video_capture = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+    '''cv2.putText(frame, 'oh my eyes are rotten ', (255, 255), font, 1, (255, 0, 0), 2)'''
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, 'oh my eyes are rotten ', (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("video", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
